499c0fbfed46a14c0d5f5347dce7012655b03ed0
Felipe FErnandes <sandyzito803@gmail.com>
2025-04-07 09:26:05 -0300

    AulaPwII

---
+++ Aula2/concatenar.php
@@ -0,0 +1,14 @@
+
+<?php
+ECHO "Hello Word!<br>";
+ECHO "Hello Word!<br>";
+ECHO "ola!<br>";
+
+$nome = "Felipe";
+$sobrenome = "Fernandes";
+$numero = 4;
+
+echo "${nome} <h1> ${sobrenome} </h1>";
+echo $numero . "<p> " . $nome .
+" " . $sobrenome . "</p>";
+?>
